43a734313ed9dc5c67f6624abdcd007d886f0612
juanuranidev <juanuranidev@gmail.com>
2023-11-18 00:18:37 -0300

    Fix

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,13 +1,7 @@
-import 'dotenv/config';
-import { get } from 'env-var';
-
+import "dotenv/config";
+import { get } from "env-var";
 
 export const envs = {
-
-  PORT: get('PORT').required().asPortNumber(),
-  PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
-
-}
-
-
-
+  PORT: get("PORT").asPortNumber(),
+  PUBLIC_PATH: get("PUBLIC_PATH").default("public").asString(),
+};
