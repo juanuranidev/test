8a0d948c165e284493ea881376299358d5793b9b
juanuranidev <juanuranidev@gmail.com>
2023-11-18 00:20:50 -0300

    Test

--- dist/config/envs.js
+++ dist/config/envs.js
@@ -4,6 +4,6 @@ exports.envs = void 0;
 require("dotenv/config");
 const env_var_1 = require("env-var");
 exports.envs = {
-    PORT: (0, env_var_1.get)('PORT').required().asPortNumber(),
-    PUBLIC_PATH: (0, env_var_1.get)('PUBLIC_PATH').default('public').asString(),
+    PORT: (0, env_var_1.get)("PORT").required().asPortNumber(),
+    PUBLIC_PATH: (0, env_var_1.get)("PUBLIC_PATH").default("public").asString(),
 };
